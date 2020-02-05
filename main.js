0cb3fa6f2b7cace65a03b5fae04da32fe985eede
AsociTon <asociton@outlook.com>
2020-02-05 16:33:43 +0530

    Add base for renderer and video playback.
    TODO:
    Run python script of the model for node.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const electron = require('electron');
+
 const url = require('url');
 const path = require('path');
-
+const fs = require('fs');
 const {app, BrowserWindow} = electron;
+const ipc = require('electron').ipcMain;
+
+const express = require('express');
+const app = express();
+const http = require("http");
+
+app.getAppMetrics("/", function(req, res){
+    
+})
 
 let mainWindow;
-
 
 function createWindow(){
     mainWindow = new BrowserWindow({
@@ -16,7 +25,10 @@ function createWindow(){
         }
     })
 
-    mainWindow.loadFile('index.html')
+    // Base html doc
+    mainWindow.loadFile('public/index.html')
+    mainWindow.removeMenu();
+
 }
 
 app.on('ready', createWindow)
