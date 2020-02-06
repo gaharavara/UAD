57a4a3c03ec4c7805ea3048ae987c16d38ae5ef2
AsociTon <asociton@outlook.com>
2020-02-06 07:03:23 +0530

    Add YOLO v1 implementation on Electron GUI.
    Contains:
    * Browse and select any video from local/system.
    * Identify cars in the selected test video.

--- main.js
+++ main.js
@@ -3,16 +3,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 const {app, BrowserWindow} = electron;
 const ipc = require('electron').ipcMain;
-
-const express = require('express');
-const app = express();
-const http = require("http");
-
-app.getAppMetrics("/", function(req, res){
-    
-})
+const {dialog} = require('electron')                 
 
 let mainWindow;
 
@@ -21,14 +15,34 @@ function createWindow(){
         width: 1024,
         height: 768,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         }
     })
 
     // Base html doc
+    console.log("Loading index.html");
     mainWindow.loadFile('public/index.html')
+    // Omit default menu bar
     mainWindow.removeMenu();
+ 
+    // Waiting for the path of the video from the user
+    ipc.on('send-path', (event, args) => {
+
+
+        console.log("Request from GUI recieved." + args);
+        
+        // Passing path of the video/footage to python script
+        var python = require('child_process').spawn('python', ['./yolo/realtime_detect.py', args], {shell: true, detached: true});
+        console.log("args: ", args);
+
+        // Recieving the output from python script
+        python.stdout.on('data',function(data){
+           console.log("data: ",data.toString('utf8'));
+        });
+    })
 
 }
 
+
 app.on('ready', createWindow)
+
--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,1 +1,70 @@
 const ipc = require('electron').ipcRenderer;
+const fs = require('fs');
+// To play selected videos
+
+(function localFileVideoPlayer() {
+    'use strict'
+    var URL = window.URL || window.webkitURL
+    var displayMessage = function (message, isError) {
+        var element = document.querySelector('#message')
+        element.innerHTML = message
+        element.className = isError ? 'error' : 'info'
+    }
+    var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var path = this.files[0].path;
+    console.log(path);
+
+    //Ping main process
+    console.log(ipc.send('send-path', path));
+    
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+    return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+    }
+    var inputNode = document.querySelector('input')
+    inputNode.addEventListener('change', playSelectedFile, false)   
+})()
+
+
+/*
+
+
+const {dialog} = require('electron').remote;
+console.log("Inside renderer");
+document.querySelector('#upload').addEventListener('click', function (event) {
+    console.log(dialog);
+    dialog.showOpenDialog(function(filenames){
+        if(filenames == undefined){
+            console.log("Undefined file");
+        } else {
+            console.log("Success");
+        }
+    });
+});
+
+
+*/
+
+
+const buttonCreated = document.getElementById('upload');
+
+buttonCreated.addEventListener('click', function (event) {
+    console.log("Button Clicked")
+    ipc.send('open-file-dialog-for-file')
+});
+
+ipc.on('selected-file', function (event, path) {
+    console.log('Full path: ', path);
+});
